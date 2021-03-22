52f09bfd10411ea265903dc84b4391c4d4f6c924
Tal Danzig <tdanzig@cisco.com>
2021-03-22 00:10:24 -0700

    deal with funky chars

--- container/start.sh
+++ container/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cd /usr/local/nginx/conf/
+cd /usr/local/nginx/conf/ || exit
 
 cp nginx.conf.in nginx.conf
+YOUTUBE_KEY="$(printf '%s\n' "$YOUTUBE_KEY" | sed -e 's/[\/&]/\\&/g')"
+FB_KEY="$(printf '%s\n' "$FB_KEY" | sed -e 's/[\/&]/\\&/g')"
+PASSWORD="$(printf '%s\n' "$PASSWORD" | sed -e 's/[\/&]/\\&/g')"
 sed -i -e "s/__FB_KEY__/${FB_KEY}/" -e "s/__YOUTUBE_KEY__/${YOUTUBE_KEY}/" -e "s/__PASSWORD__/${PASSWORD}/" nginx.conf
 
 echo "======>>>> BEGIN CONF <<<<========="
